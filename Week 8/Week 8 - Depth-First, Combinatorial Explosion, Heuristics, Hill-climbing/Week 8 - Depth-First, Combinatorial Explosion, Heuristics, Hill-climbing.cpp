c2066cdcca6982b2abfb6bd125154c6ba57b0c3e
cogh <coghdevelops@gmail.com>
2019-11-14 16:51:00 +0000

    Started Week 8

---
+++ Week 8/Week 8 - Depth-First, Combinatorial Explosion, Heuristics, Hill-climbing/Week 8 - Depth-First, Combinatorial Explosion, Heuristics, Hill-climbing.cpp
@@ -0,0 +1,23 @@
+// Week 8 - Depth-First, Combinatorial Explosion, Heuristics, Hill-climbing.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <memory>
+
+class Coords {
+	int x;
+	int y;
+};
+
+int main()
+{
+	// Greeting
+    std::cout << "Hello World!\n";
+
+	// Exercise 1
+	Coords* pt = new Coords;
+
+
+	// Stall
+	system("pause");
+}
